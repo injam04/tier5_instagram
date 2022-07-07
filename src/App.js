aed5262a6a6f40c79ccf4c7ac283f00477823a2c
injam04 <injamam@amarlab.com>
2022-07-07 18:35:27 +0600

    loading added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import Loading from './components/Loading';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
 class App extends Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 1000);
+  }
+
   render() {
+    const { loading } = this.state;
+
     return (
       <>
-        <Navbar />
-        <Home />
+        {loading ? (
+          <>
+            <Loading />
+          </>
+        ) : (
+          <>
+            <Navbar />
+            <Home />
+          </>
+        )}
       </>
     );
   }
---
+++ src/components/Loading.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Logo from '../assets/img/logo.png';
+import MetaLogo from '../assets/img/Instagram-Meta.png';
+
+class Loading extends Component {
+  render() {
+    return (
+      <div className='main-loading'>
+        <img src={Logo} alt='' className='main-logo' />
+        <div className='bottom'>
+          <p className='fs-14 fw-600 text-gray m-0 text-center'>from</p>
+          <img src={MetaLogo} alt='' />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Loading;
