a69bdc7251f0495f1516961e24c08b25a8fc83bb
injam04 <injamam@amarlab.com>
2022-07-07 14:42:30 +0600

    post section partially done

---
+++ src/components/home/ProfileSec.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProfileSec extends Component {
+  render() {
+    return (
+      <>
+        <h1>cdfddf</h1>
+      </>
+    );
+  }
+}
+
+export default ProfileSec;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import PostSec from '../components/home/PostSec';
+import ProfileSec from '../components/home/ProfileSec';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>This Is Homepage</h1>
-      </div>
+      <section className='homepage'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-md-7'>
+              <div className='main-sec'>
+                <PostSec />
+              </div>
+            </div>
+            <div className='col-md-5'>
+              <div className='profile-sec'>
+                <ProfileSec />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
